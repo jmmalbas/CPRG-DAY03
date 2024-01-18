1ca68d37ae1a94be9939f9671f7126a4f6071da0
John Michael Malbas <923415@ACDM.DS.SAIT.CA>
2024-01-18 09:11:34 -0700

    Added some styles

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtPage />
+    <AppHeader />
+  <NuxtHeader />
   </div>
 </template>
---
+++ nuxt-app/components/appheader.vue
@@ -0,0 +1,19 @@
+<template>
+<header>
+  <div>
+    <NuxtLink to = "/">Logo: User Name</NuxtLink>> 
+    <div>
+      <nav>
+         <NuxtLink to = "signup"> SIGNUP PAGE<NuxtLink>
+      </nav>
+</header>
+</template>
+<style>
+<header> {
+  display: flex;
+  justify-content: space-between;
+  gap
+}
+
+</header>
+</style>
